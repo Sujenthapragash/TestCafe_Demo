ac9a5f12d0b8a538deea90978410e26f5020cb70
Sujen <sujen06@hotmail.com>
2022-12-10 00:41:22 +1100

    test commit

--- test/firstTest.js
+++ test/firstTest.js
@@ -1,7 +1,7 @@
 
 fixture("First Fixture")
     .page("https://devexpress.github.io/testcafe/example/");
-@test comment
+@test comment of update
 test("First test", async t => {
     await t
         .typeText("#developer-name", "TAU")
